d352f566365d9b3568f2ec359b000b05f7cbf003
Сергей Субботин <zarodiny@yandex.ru>
2023-09-27 20:48:15 +1000

    feat: добавляет стили меню

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import { initNav } from "./module/nav.js";
 import { initSlider } from "./module/sliders.js";
+import { initCurrentYear } from "./module/current-year.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('подключен скрипт main.js');
 
     initNav();
     initSlider();
+    initCurrentYear();
     baguetteBox.run('.gallery-wrapper');
 
     // DOMContentLoaded
