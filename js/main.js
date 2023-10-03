88cc9b05c2894a504aff1025f7946465c676b70d
Сергей Субботин <zarodiny@yandex.ru>
2023-10-03 18:07:33 +1000

    feat: добавляет скролл

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { initNav } from "./module/nav.js";
 import { initSlider } from "./module/sliders.js";
 import { initCurrentYear } from "./module/current-year.js";
 import { initScrollToTop } from "./module/scroll-to-top.js";
+import { initScroll } from "./module/scroll.js";
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('подключен скрипт main.js');
@@ -10,6 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
     initSlider();
     initCurrentYear();
     initScrollToTop();
+    initScroll();
     baguetteBox.run('.gallery-wrapper');
 
     // DOMContentLoaded
